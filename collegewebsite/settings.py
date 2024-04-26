30733ff2f6d3eebb6947810f7a78b86fac6399d3
Vineet Jassal <vineetjassal35@gmail.com>
2024-04-26 21:39:25 +0530

    Update settings.py

--- collegewebsite/settings.py
+++ collegewebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$xwhnwp#(u1a@6cfcvryq8yyaind9!ku6-_*a2r0pgo#lqun4g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 STATICFILES_DIRS = [
       "static"
